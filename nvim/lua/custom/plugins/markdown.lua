94a3ba2c12af520c2491a27145e80a9265b92b82
Minjie Ni <minjie.ni@booking.com>
2026-04-10 13:02:55 +0800

    update

---
+++ nvim/lua/custom/plugins/markdown.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    ft = { 'markdown' },
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+    opts = {},
+  },
+  {
+    'iamcco/markdown-preview.nvim',
+    ft = { 'markdown' },
+    build = 'cd app && npx --yes yarn install',
+    keys = {
+      { '<leader>mp', '<cmd>MarkdownPreviewToggle<cr>', desc = '[M]arkdown [P]review' },
+    },
+  },
+}
